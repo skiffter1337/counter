a10928b5cea05154b9f75991d60f7c968811d55c
Ilya Shulapov <shulapov1999@gmail.com>
2023-02-25 18:54:38 +0300

    add component that including counter settings, add initial state to maxValue

---
+++ src/Components/Counter.tsx
@@ -0,0 +1,32 @@
+import React, {useState} from 'react';
+import s from "./Counter.module.css"
+import {SuperButton} from "./SuperButton/SuperButton";
+import {SettingsScreen} from "./SettingsScreen/SettingsScreen";
+
+export const Counter = () => {
+
+    const startCounterValue = 0
+    const maxCounterValue = 5
+    const [maxValue, setMaxValue] = useState<number>(maxCounterValue)
+    const [startValue, setStartValue] = useState<number>(startCounterValue)
+    // const setFromLocalStorage = () => localStorage.setItem("counterValue", JSON.stringify(startValue))
+
+
+
+    return (
+        <div className={s.counter}>
+            <SettingsScreen/>
+        <div className={s.counterScreen}>
+            <div className={s.count}>
+                <div className={startValue >= maxCounterValue ? s.countError : s.countNoError}>
+                    <span>{startValue}</span>
+                </div>
+            </div>
+            <div className={s.buttons}>
+                <SuperButton callback={() => setStartValue(startValue + 1)} disabled={startValue >= maxCounterValue}>inc</SuperButton>
+                <SuperButton callback={() => setStartValue(startCounterValue)} disabled={startValue === startCounterValue}>reset</SuperButton>
+            </div>
+        </div>
+        </div>
+    );
+};
--- src/Components/SuperButton/SuperButton.tsx
+++ src/Components/SuperButton/SuperButton.tsx
@@ -3,12 +3,12 @@ import React from 'react';
 type SuperButtonType = {
     callback: ()=>void
     children?: React.ReactNode
-    disabled: boolean
-    count: number
+    disabled?: boolean
+
 }
 
 export const SuperButton: React.FC<SuperButtonType> = (props) => {
-    const {callback, children, count, disabled,  ...otherprops} = props
+    const {callback, children, disabled,  ...otherprops} = props
 
     const onClickCallback = () => callback()
 
