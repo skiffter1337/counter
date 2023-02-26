c1a7780aa603cc115889cb1aa4b3a37a3687ba8f
Ilya Shulapov <shulapov1999@gmail.com>
2023-02-26 16:26:16 +0300

    add Min/max input logic

--- src/Components/Counter.tsx
+++ src/Components/Counter.tsx
@@ -1,42 +1,57 @@
-import React, {useEffect, useState} from 'react';
+import React, {ChangeEvent, useEffect, useState} from 'react';
 import s from "./Counter.module.css"
 import {SuperButton} from "./SuperButton/SuperButton";
-import {SettingsScreen} from "./SettingsScreen/SettingsScreen";
+
 
 export const Counter = () => {
 
-    const startCounterValue = 0
-    const maxCounterValue = 5
+    let minCounterValue = 0
+    let maxCounterValue = 5
+    const [minValue, setMinValue] = useState<number>(minCounterValue)
     const [maxValue, setMaxValue] = useState<number>(maxCounterValue)
-    const [startValue, setStartValue] = useState<number>(startCounterValue)
+
+    const [minInput, setMinInput] = useState(minValue)
+    const [maxInput, setMaxInput] = useState(maxValue)
 
     useEffect(() => {
-        let valueAsString= localStorage.getItem("counterValue")
+        let valueAsString = localStorage.getItem("counterValue")
         if (valueAsString) {
             let parsedValue = JSON.parse(valueAsString)
-            setStartValue(parsedValue)
+            setMinValue(parsedValue)
         }
     }, [])
-    useEffect(() => localStorage.setItem("counterValue", JSON.stringify(startValue)), [startValue])
+    useEffect(() => localStorage.setItem("counterValue", JSON.stringify(minValue)), [minValue])
 
+
+    const setMinMaxValue = () => {
+        setMinValue(minInput)
+        setMaxValue(maxInput)
+    }
+
+    const onChangeMinInputHandler = (e: ChangeEvent<HTMLInputElement>) => setMinInput(JSON.parse(e.currentTarget.value))
+    const onChangeMaxInputHandler = (e: ChangeEvent<HTMLInputElement>) => setMaxInput(JSON.parse(e.currentTarget.value))
 
 
     return (
         <div className={s.counter}>
-
-            <SettingsScreen/>
-
+            <div className={s.settings}>
+                Min:
+                <input value={minInput} onChange={onChangeMinInputHandler} className={s.inputs} type={"number"}/>
+                Max:
+                <input  value={maxInput} onChange={onChangeMaxInputHandler} className={s.inputs} type={"number"}/>
+                <div className={s.setButton}><SuperButton callback={setMinMaxValue}>set</SuperButton></div>
+            </div>
             <div className={s.counterScreen}>
                 <div className={s.count}>
-                    <div className={startValue >= maxCounterValue ? s.countError : s.countNoError}>
-                        <span>{startValue}</span>
+                    <div className={minValue >= maxValue ? s.countError : s.countNoError}>
+                        <span>{minValue}</span>
                     </div>
                 </div>
                 <div className={s.buttons}>
-                    <SuperButton callback={() => setStartValue(startValue + 1)}
-                                 disabled={startValue >= maxCounterValue}>inc</SuperButton>
-                    <SuperButton callback={() => setStartValue(startCounterValue)}
-                                 disabled={startValue === startCounterValue}>reset</SuperButton>
+                    <SuperButton callback={() => setMinValue(minValue + 1)}
+                                 disabled={minValue >= maxValue}>inc</SuperButton>
+                    <SuperButton callback={() => setMinValue(minCounterValue)}
+                                 disabled={minValue === minCounterValue}>reset</SuperButton>
                 </div>
             </div>
 
