deffd25dd2c2a0325c0f3e4a60597e74049315ee
Ilya Shulapov <shulapov1999@gmail.com>
2023-02-26 10:51:38 +0300

    add localstorage

--- src/Components/Counter.tsx
+++ src/Components/Counter.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import s from "./Counter.module.css"
 import {SuperButton} from "./SuperButton/SuperButton";
 import {SettingsScreen} from "./SettingsScreen/SettingsScreen";
@@ -9,24 +9,37 @@ export const Counter = () => {
     const maxCounterValue = 5
     const [maxValue, setMaxValue] = useState<number>(maxCounterValue)
     const [startValue, setStartValue] = useState<number>(startCounterValue)
-    // const setFromLocalStorage = () => localStorage.setItem("counterValue", JSON.stringify(startValue))
+
+    useEffect(() => {
+        let valueAsString= localStorage.getItem("counterValue")
+        if (valueAsString) {
+            let parsedValue = JSON.parse(valueAsString)
+            setStartValue(parsedValue)
+        }
+    }, [])
+    useEffect(() => localStorage.setItem("counterValue", JSON.stringify(startValue)), [startValue])
 
 
 
     return (
         <div className={s.counter}>
+
             <SettingsScreen/>
-        <div className={s.counterScreen}>
-            <div className={s.count}>
-                <div className={startValue >= maxCounterValue ? s.countError : s.countNoError}>
-                    <span>{startValue}</span>
+
+            <div className={s.counterScreen}>
+                <div className={s.count}>
+                    <div className={startValue >= maxCounterValue ? s.countError : s.countNoError}>
+                        <span>{startValue}</span>
+                    </div>
+                </div>
+                <div className={s.buttons}>
+                    <SuperButton callback={() => setStartValue(startValue + 1)}
+                                 disabled={startValue >= maxCounterValue}>inc</SuperButton>
+                    <SuperButton callback={() => setStartValue(startCounterValue)}
+                                 disabled={startValue === startCounterValue}>reset</SuperButton>
                 </div>
             </div>
-            <div className={s.buttons}>
-                <SuperButton callback={() => setStartValue(startValue + 1)} disabled={startValue >= maxCounterValue}>inc</SuperButton>
-                <SuperButton callback={() => setStartValue(startCounterValue)} disabled={startValue === startCounterValue}>reset</SuperButton>
-            </div>
-        </div>
+
         </div>
     );
 };
