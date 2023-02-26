a3729a5b8bdf0afa6fabdfe9adcf98b4f0fa7176
Ilya Shulapov <shulapov1999@gmail.com>
2023-02-26 16:56:02 +0300

    add Min/max input logic

--- src/Components/Counter.tsx
+++ src/Components/Counter.tsx
@@ -5,8 +5,10 @@ import {SuperButton} from "./SuperButton/SuperButton";
 
 export const Counter = () => {
 
+    let startCounterValue = 0
     let minCounterValue = 0
     let maxCounterValue = 5
+    const [startValue, setStartValue] = useState<number>(startCounterValue)
     const [minValue, setMinValue] = useState<number>(minCounterValue)
     const [maxValue, setMaxValue] = useState<number>(maxCounterValue)
 
@@ -24,34 +26,35 @@ export const Counter = () => {
 
 
     const setMinMaxValue = () => {
-        setMinValue(minInput)
+        setStartValue(minInput)
         setMaxValue(maxInput)
     }
 
-    const onChangeMinInputHandler = (e: ChangeEvent<HTMLInputElement>) => setMinInput(JSON.parse(e.currentTarget.value))
     const onChangeMaxInputHandler = (e: ChangeEvent<HTMLInputElement>) => setMaxInput(JSON.parse(e.currentTarget.value))
+    const onChangeMinInputHandler = (e: ChangeEvent<HTMLInputElement>) => setMinInput(JSON.parse(e.currentTarget.value))
+
 
 
     return (
         <div className={s.counter}>
             <div className={s.settings}>
+                Max:
+                <input value={maxInput} onChange={onChangeMaxInputHandler} className={s.inputs} type={"number"}/>
                 Min:
                 <input value={minInput} onChange={onChangeMinInputHandler} className={s.inputs} type={"number"}/>
-                Max:
-                <input  value={maxInput} onChange={onChangeMaxInputHandler} className={s.inputs} type={"number"}/>
                 <div className={s.setButton}><SuperButton callback={setMinMaxValue}>set</SuperButton></div>
             </div>
             <div className={s.counterScreen}>
                 <div className={s.count}>
-                    <div className={minValue >= maxValue ? s.countError : s.countNoError}>
-                        <span>{minValue}</span>
+                    <div className={startValue >= maxValue ? s.countError : s.countNoError}>
+                        <span>{startValue}</span>
                     </div>
                 </div>
                 <div className={s.buttons}>
-                    <SuperButton callback={() => setMinValue(minValue + 1)}
-                                 disabled={minValue >= maxValue}>inc</SuperButton>
-                    <SuperButton callback={() => setMinValue(minCounterValue)}
-                                 disabled={minValue === minCounterValue}>reset</SuperButton>
+                    <SuperButton callback={() => setStartValue(startValue + 1)}
+                                 disabled={startValue >= maxValue}>inc</SuperButton>
+                    <SuperButton callback={() => setStartValue(minInput)}
+                                 disabled={startValue === startCounterValue}>reset</SuperButton>
                 </div>
             </div>
 
