cecf59b9334e4126916d1e3c452419ce7b606d91
Ilya <shulapov1999@gmail.com>
2023-04-06 14:49:00 +0300

    refactoring code

--- src/Components/Counter.tsx
+++ src/Components/Counter.tsx
@@ -1,12 +1,26 @@
 import React, {ChangeEvent, useEffect, useState} from 'react';
 import s from "./Counter.module.css"
 import {SuperButton} from "./SuperButton/SuperButton";
+import {useDispatch, useSelector} from "react-redux";
+import {AppStoreType} from "../Redux/store/store";
+import {
+    changeMaxInputAC,
+    changeMinInputAC,
+    disableButtonAC,
+    incrementAC,
+    resetAC,
+    StateType
+} from "../Redux/reducers/counterReducer";
+
 
 const StartCounterValue = 0
 const MinCounterValue = 0
 const MaxCounterValue = 5
 
 export const Counter = () => {
+
+    const dispatch = useDispatch()
+    const counterValues = useSelector<AppStoreType, StateType>((state) => state.counter)
 
     const [startValue, setStartValue] = useState(StartCounterValue)
     const [minValue, setMinValue] = useState(MinCounterValue)
@@ -20,53 +34,60 @@ export const Counter = () => {
     const [onChangeBtnDisable, setOnChangeBtnDisable] = useState(false)
     const [enterValueMessage, setEnterValueMessage] = useState(false)
 
-    useEffect(() => {
-        let valueAsString = localStorage.getItem("counterValue")
-        if (valueAsString) {
-            let parsedValue = JSON.parse(valueAsString)
-            setStartValue(parsedValue)
-        }
-    }, [])
-    useEffect(() => localStorage.setItem("counterValue", JSON.stringify(startValue)), [startValue])
+    // useEffect(() => {
+    //     let valueAsString = localStorage.getItem("counterValue")
+    //     if (valueAsString) {
+    //         let parsedValue = JSON.parse(valueAsString)
+    //         setStartValue(parsedValue)
+    //     }
+    // }, [])
+    // useEffect(() => localStorage.setItem("counterValue", JSON.stringify(startValue)), [startValue])
+    // useEffect(() => {
+    //     let valueAsString = localStorage.getItem("minInputValue")
+    //     if (valueAsString) {
+    //         let parsedValue = JSON.parse(valueAsString)
+    //         setMinInput(parsedValue)
+    //     }
+    // }, [])
+    // useEffect(() => localStorage.setItem("minInputValue", JSON.stringify(minInput)), [minInput])
+    // useEffect(() => {
+    //     let valueAsString = localStorage.getItem("maxInputValue")
+    //     if (valueAsString) {
+    //         let parsedValue = JSON.parse(valueAsString)
+    //         setMaxInput(parsedValue)
+    //     }
+    // }, [])
+    // useEffect(() => localStorage.setItem("maxInputValue", JSON.stringify(maxInput)), [maxInput])
+    // localstorage
 
-    useEffect(() => {
-        let valueAsString = localStorage.getItem("minInputValue")
-        if (valueAsString) {
-            let parsedValue = JSON.parse(valueAsString)
-            setMinInput(parsedValue)
-        }
-    }, [])
-    useEffect(() => localStorage.setItem("minInputValue", JSON.stringify(minInput)), [minInput])
-
-    useEffect(() => {
-        let valueAsString = localStorage.getItem("maxInputValue")
-        if (valueAsString) {
-            let parsedValue = JSON.parse(valueAsString)
-            setMaxInput(parsedValue)
-        }
-    }, [])
-    useEffect(() => localStorage.setItem("maxInputValue", JSON.stringify(maxInput)), [maxInput])
 
     useEffect(() => {
         if (inputsOnchangeParameters) {
-            setOnChangeBtnDisable(true)
+            dispatch(disableButtonAC())
             setEnterValueMessage(true)
         } else {
             setOnChangeBtnDisable(false)
             setEnterValueMessage(false)
         }
     }, [minInput, maxInput])
+
     useEffect(() => inputsDisableConditions ? setError(true) : setError(false), [minInput, maxInput])
 
-    const inputsOnchangeParameters = minInput !== minValue || maxInput !== maxValue;
+    const inputsOnchangeParameters = counterValues.minInput !== counterValues.minValue || counterValues.maxInput !== counterValues.maxValue;
     const inputsDisableConditions = minInput < 0 || maxInput <= minInput
+
     const enterMessage = enterValueMessage && "enter values and press 'set'"
     const errorMessage = error && "Incorrect value!"
+
+    const countValueVariants = errorMessage ? errorMessage : enterMessage ? enterMessage : startValue
+
     const maxInputClasses = `${s.inputs} ${s.minInput} ${maxInput <= minInput ? s.InputErr : ""}`
     const minInputClasses = `${s.inputs} ${minInput < 0 || minInput === maxInput ? s.InputErr : ""}`
     const countClasses = `${startValue >= maxValue ? s.settingsOnChangeMessage : ""} ${errorMessage ? s.errorMessage : ""}`
     const countValueClasses = `${!errorMessage && !enterMessage ? s.countValue : ""}`
-    const countValueVariants = errorMessage ? errorMessage : enterMessage ? enterMessage : startValue
+
+
+
 
     const setMinMaxValue = () => {
         setStartValue(minInput)
@@ -75,8 +96,24 @@ export const Counter = () => {
         setEnterValueMessage(false)
     }
 
-    const onChangeMaxInputHandler = (e: ChangeEvent<HTMLInputElement>) => setMaxInput(JSON.parse(e.currentTarget.value))
-    const onChangeMinInputHandler = (e: ChangeEvent<HTMLInputElement>) => setMinInput(JSON.parse(e.currentTarget.value))
+    const increment = () => {
+        dispatch(incrementAC())
+        // setStartValue(startValue + 1)
+    }
+     const reset = () => {
+        dispatch(resetAC())
+         // setStartValue(minInput)
+    }
+
+    const changeMaxInput = (e: ChangeEvent<HTMLInputElement>) => {
+        dispatch(changeMaxInputAC(JSON.parse(e.currentTarget.value)))
+    }
+    const changeMinInput = (e: ChangeEvent<HTMLInputElement>) => {
+        dispatch(changeMinInputAC(JSON.parse(e.currentTarget.value)))
+    }
+
+    // const onChangeMaxInputHandler = (e: ChangeEvent<HTMLInputElement>) => setMaxInput(JSON.parse(e.currentTarget.value))
+    // const onChangeMinInputHandler = (e: ChangeEvent<HTMLInputElement>) => setMinInput(JSON.parse(e.currentTarget.value))
 
     return (
         <div className={s.counter}>
@@ -86,7 +123,7 @@ export const Counter = () => {
                         <span className={s.inputValue}>Max value:</span>
                         <input
                             value={maxInput}
-                            onChange={onChangeMaxInputHandler}
+                            onChange={changeMaxInput}
                             className={maxInputClasses}
                             type={"number"}/>
                     </div>
@@ -94,7 +131,7 @@ export const Counter = () => {
                         <span className={s.inputValue}>Min value: </span>
                         <input
                             value={minInput}
-                            onChange={onChangeMinInputHandler}
+                            onChange={changeMinInput}
                             className={minInputClasses}
                             type={"number"}/>
                     </div>
@@ -119,12 +156,12 @@ export const Counter = () => {
                 <div className={s.counterButtons}>
                     <div className={s.buttons}>
                         <div className={s.incButton}>
-                            <SuperButton callback={() => setStartValue(startValue + 1)}
-                                         disabled={startValue >= maxValue || onChangeBtnDisable}>inc</SuperButton>
+                            <SuperButton callback={increment}
+                                         disabled={startValue >= maxValue || counterValues.buttonDisable}>inc</SuperButton>
                         </div>
                         <div className={s.resetButton}>
-                            <SuperButton callback={() => setStartValue(minInput)}
-                                         disabled={startValue === StartCounterValue || onChangeBtnDisable}>reset</SuperButton>
+                            <SuperButton callback={reset}
+                                         disabled={startValue === StartCounterValue || counterValues.buttonDisable}>reset</SuperButton>
                         </div>
                     </div>
                 </div>
